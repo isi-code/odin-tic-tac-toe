02568b95c91466456dd00e57922d1fc42bab2bb7
isi-code <irvinjose.silva@gmail.com>
2025-11-12 16:32:32 -0800

    Add method to update player choices

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
 function player(name,icon) {
     const name = name;
-    let name = icon;
+    const icon = icon;
     const choices = [];
+
+    const updateChoice = (choice) => { 
+        choices.push(choice);
+       }
+
+    return { updateChoice }
 }
 
 function gameBoard() {
     const gameBoard = new Array("","","","","","","","","");
     let playerTurn = "";
     let gameOver = false;
-
 }
+
+let player1Name = input("What is Player1 name?");
+let player2Name = input("What is Player2 name?");
+
+const player1 = player(player1Name,"O")
+const player2 = player(player2Name,"X")
+
+let question = input("Choose a number from 1 - 9: ");
+player1.updateChoice(question);
+question = input("Choose a number from 1 - 9: ");
+player2.updateChoice(question);
