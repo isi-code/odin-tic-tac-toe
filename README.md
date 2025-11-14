9a5b4cfa9c84cfbd546916cf69822313e039134c
isi-code <irvinjose.silva@gmail.com>
2025-11-13 19:32:15 -0800

    Update Tic Tac Toe game plan

--- README.md
+++ README.md
@@ -24,27 +24,27 @@ Create a Tic Tac Toe game with JavaScript CSS and HTML.
 **Player**
 
 - Player's name and icon property.
-- Array with player's valid choices property (must be numbers from 1 - 9).
+- Array that stores player's valid choices property (must be numbers from 0 - 8).
 - Method to update choices array and prevent duplicates or wrong values.
+- Method to set player icon and name.
 - Methods that returns icon, name and choices
 
 **Board**
 
 - Array property with 9 spaces representing the board
-- Method to prevent playing the same square
 - Method that will update the game board
+- Method that return current board
 
 ***controlGameFlow**
-- Add property to indicate player's turn
 - Add property to check if game is over
 - Method to 
 - Method that checks whether the win or tie conditions are met
   - **Win conditons**
-    - (1,2,3)
-    - (4,5,6)
-    - (7,8,9)
-    - (1,5,9)
-    - (3,5,7)
+    - (0,1,2)
+    - (3,4,5)
+    - (6,7,8)
+    - (0,4,8)
+    - (2,4,6)
   - **Tie conditions**
     - All nine spaces are marked no condition above is met by a single player
  
--- script.js
+++ script.js
@@ -1,28 +1,62 @@
-function player(name,icon) {
-    const name = name;
-    const icon = icon;
+function player () {
+    let name = "";
+    let icon = "";
     const choices = [];
 
-    const updateChoice = (choice) => { 
-        choices.push(choice);
-       }
+    const updateChoices = (choice) => {
+        if ( choices.includes(choice) && (choice <= 8 && choice > 0 ) ) {
+            choices.push(choice);
+        }
+       };
 
-    return { updateChoice }
+    const setIcon = (playerIcon) => { icon = playerIcon };
+
+    const setname = (playerName) => { name = playerName };
+
+    const getIcon = () => { return icon };
+
+    const getName = () => { return name };
+
+    return { updateChoices, setIcon, setname, getIcon, getName }
 }
 
-function gameBoard() {
+const gameBoard = (function () {
     const gameBoard = new Array("","","","","","","","","");
-    let playerTurn = "";
-    let gameOver = false;
-}
 
-let player1Name = input("What is Player1 name?");
-let player2Name = input("What is Player2 name?");
+    const updateBoard = (index) => {
+        gameBoard[index]
+    }
 
-const player1 = player(player1Name,"O")
-const player2 = player(player2Name,"X")
+    const getGameBoard = () => { return gameBoard }
 
-let question = input("Choose a number from 1 - 9: ");
-player1.updateChoice(question);
-question = input("Choose a number from 1 - 9: ");
-player2.updateChoice(question);
+    return { updateBoard, getGameBoard }
+})()
+
+const gameLogic = (function () {
+    const player1 = player();
+    const player2 = player();
+    //let gameOver = false;
+
+    const askPlayerChoice = () => {
+        let askNumber;
+        do {
+            askNumber = parseInt(prompt("Type a number (1-8): "));
+        } while ( typeof askNumber === "number" && gae );
+
+        gameBoard.updateBoard(askNumber);
+        return askNumber
+    }
+
+    const isGameOver = () => {
+        const currentBoardState = gameBoard.getGameBoard();
+
+        if (!currentBoardState.inludes("")){
+            console.log("It's a tie");
+            return false
+        }
+
+    }
+
+    return { isGameOver }
+
+})(playerName1,playerName2)
